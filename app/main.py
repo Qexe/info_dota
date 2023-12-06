7ed5a4535ea14d694fa47f3ff0a77b6b03fcc1af
Radomir Popov <r.popov@brandquad.ru>
2023-12-07 00:45:25 +0300

    next version

--- app/main.py
+++ app/main.py
@@ -8,28 +8,28 @@ import json
 
 import requests
 
-import psycopg2
+import asyncio
+
+import asyncpg
 
 app = FastAPI()
 
 
 @app.get("/players/{player_id}")
-def get_player_info(player_id):
+async def get_player_info(player_id):
     api_url = f"https://api.opendota.com/api/players/{player_id}"
     res = requests.get(url=api_url)
     print(res)
-    rec = Db(url=api_url, query_id=player_id, res=res).record_request(
+    await Db(url=api_url, query_id=player_id, res=res).record_request()
     return res.json
 
 @app.get("/matches/{match_id}")
-def get_match_info(match_id):
+async def get_match_info(match_id):
     api_url = f"https://api.opendota.com/api/matches/{match_id}"
     res = requests.get(url=api_url)
     print(res)
-    rec = Db(query=api_url, query_id=match_id, res=res).record_request()
+    await Db(url=api_url, query_id=match_id, res=res).record_request()
     return res.json()
-
-
 
 class Db:
 
@@ -38,13 +38,11 @@ class Db:
       self.res = res
       self.url = url
 
-    def record_request(self):
-     conn = psycopg2.connect(host='localhost', port='5432', database = 'infodota_db', user = 'app', password = '3226')
-     cur = conn.cursor()
+    async def record_request(self):
+     conn = await asyncpg.connect(host='localhost', port='5432', database='infodota_db', user='app', password='3226')
      main = (self.url, self.query_id, self.res.text)
-     cur.execute("INSERT INTO main_data (url, request_body, response) VALUES (%s,%s,%s);", main)
+     await conn.execute("INSERT INTO main_data (url, request_body, response) VALUES (%s,%s,%s);", main)
      metadata = (self.res.headers['date'], self.res.status_code, self.res.elapsed.total_seconds())
-     cur.execute("INSERT INTO metadata (date, status, req_timing) VALUES (%s,%s,%s);", metadata)
+     await conn.execute("INSERT INTO metadata (date, status, req_timing) VALUES (%s,%s,%s);", metadata)
      conn.commit()
-     cur.close()
-     conn.close()
+     await conn.close()
